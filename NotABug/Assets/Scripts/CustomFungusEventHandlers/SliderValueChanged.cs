7e3cd1b40fdebe2773a9505a01019e68c4d1a838
CG-Tespy <gabriel.cabrera257@hotmail.com>
2019-10-06 05:09:19 -0400

    Implemented Sound Control Menu for title screen

---
+++ NotABug/Assets/Scripts/CustomFungusEventHandlers/SliderValueChanged.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Fungus;
+
+/// <summary>
+    /// The block will execute when the user toggles on the target UI toggle object.
+    /// </summary>
+    [EventHandlerInfo("UI",
+                      "Slider Value Changed",
+                      "The block will execute when the value of the target UI slider object changes.")]
+    [AddComponentMenu("")]
+public class SliderValueChanged : EventHandler
+{
+    [SerializeField] protected Slider targetSlider;
+    protected float SliderValue { get { return targetSlider.value; } }
+
+    [Tooltip("Variable that the new slider value gets stored in.")]
+    [VariablePropertyAttribute(typeof(FloatVariable))] 
+    [SerializeField] protected FloatVariable sliderValue;
+
+    void Awake()
+    {
+        //VariablePropertyAttribute
+        if (targetSlider != null)
+            ListenForSliderValueChange();
+    }
+
+    void ListenForSliderValueChange()
+    {
+        targetSlider.onValueChanged.AddListener(OnSliderValueChanged);
+    }
+
+    protected virtual void OnSliderValueChanged(float newValue)
+    {
+        if (targetSlider != null)
+        {
+            if (sliderValue != null)
+                sliderValue.Value = newValue;
+
+            ExecuteBlock();
+        }
+    }
+
+}
---
+++ NotABug/Assets/Scripts/SyncVolumeToSliderValue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// When a given slider's value changes, a given AudioSource's volume changes to match.
+/// </summary>
+public class SyncVolumeToSliderValue : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] Slider slider;
+
+    void Awake()
+    {
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
+    }
+
+    void OnDestroy()
+    {
+        slider.onValueChanged.RemoveListener(OnSliderValueChanged);
+    }
+
+    void OnSliderValueChanged(float newSliderValue)
+    {
+        audioSource.volume = newSliderValue;
+    }
+}
