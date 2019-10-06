09d3a2d8c2662d8240d29399d2af9e853d294c82
CG-Tespy <gabriel.cabrera257@hotmail.com>
2019-10-06 07:31:51 -0400

    Added some transitions to the title screen menus

---
+++ NotABug/Assets/Scripts/FadeCanvasGroup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+/// <summary>
+/// Like FadeUI, but only applies to CanvasGroup components, and fades only opacity.
+/// </summary>
+[CommandInfo("UI",
+                 "Fade Canvas Group",
+                 "Fades an object based using its CanvasGroup components")]
+public class FadeCanvasGroup : TweenUI
+{
+    [SerializeField] protected FloatData targetAlpha = new FloatData(1f);
+
+    CanvasGroup toFade;
+    LTDescr fadeAction, firstFadeEase, secondFadeEase;
+
+    protected override void ApplyTween(GameObject gameObject)
+    {
+        GetAppropriateCanvasGroupFrom(gameObject);
+        FadeAsAppropriate();
+    }
+
+    void GetAppropriateCanvasGroupFrom(GameObject gameObject)
+    {
+        toFade = gameObject.GetComponent<CanvasGroup>();
+    }
+
+    void FadeAsAppropriate()
+    {
+        if (ShouldFadeInstantly())
+        {
+            ApplyFadeInstantly();
+        }
+        else
+        {
+            ApplyFadeOverTime();
+        }
+    }
+
+    bool ShouldFadeInstantly()
+    {
+        return Mathf.Approximately(duration, 0f);
+    }
+
+    void ApplyFadeInstantly()
+    {
+        toFade.alpha = targetAlpha;
+    }
+
+    void ApplyFadeOverTime()
+    {
+        StartTheFadeAction();
+        EaseTheFading();
+    }
+
+    void StartTheFadeAction()
+    {
+        var canvasGroupRectTrans = toFade.GetComponent<RectTransform>();
+        fadeAction = LeanTween.alpha(canvasGroupRectTrans, targetAlpha, duration);
+    }
+
+    void EaseTheFading()
+    {
+        // The ease was applied twice like this the FadeUI component's source code, so I assume 
+        // it should be the same here.
+        fadeAction.setEase(tweenType).setEase(tweenType);
+    }
+
+    public override bool IsPropertyVisible(string propertyName)
+    {
+        return true;
+    }
+}
---
+++ NotABug/Assets/Scripts/SyncCanvasAndUIAlphas.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// When an object's Canvas Group alpha changes, any alpha values on other UI components
+/// (like Images) are made to be the same.
+/// </summary>
+public class SyncCanvasAndUIAlphas : MonoBehaviour
+{
+    [SerializeField] CanvasGroup canvasGroup;
+    [SerializeField] GameObject hasUIElements;
+    Graphic[] uiElements;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SyncAlphas()
+    {
+
+    }
+}
