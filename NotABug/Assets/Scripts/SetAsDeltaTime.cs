a488c3449111e5320a87049bad222517827ea7ec
CG-Tespy <gabriel.cabrera257@hotmail.com>
2019-10-07 14:03:44 -0400

    Implemented a healthbar that fills smoothly

---
+++ NotABug/Assets/Scripts/SetAsDeltaTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Fungus;
+
+[CommandInfo("Variable", 
+            "Set As DeltaTime", 
+            "Sets the selected FloatVariable as equal to Time.deltaTime.")]
+[AddComponentMenu("")]
+public class SetAsDeltaTime : Command
+{
+    [SerializeField] FloatData varToSet;
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        varToSet.Value = Time.deltaTime;
+    }
+}
---
+++ NotABug/Assets/Scripts/SetFillValue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+[CommandInfo("UI", 
+            "Set Fill Value", 
+            "Sets the fill value of a filled Image component.")]
+[AddComponentMenu("")]
+public class SetFillValue : Command
+{
+    [SerializeField] Image filledImage;
+    [SerializeField] FloatData newFillValue = new FloatData(1f);
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+
+        filledImage.fillAmount = newFillValue.Value;
+        Continue();
+    }
+}
