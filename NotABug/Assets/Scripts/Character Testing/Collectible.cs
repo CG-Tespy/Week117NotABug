d026becd5149e63c1d628189914e5505c7c8854c
Nate Karabensh <natekarabensh@gmail.com>
2019-10-07 23:44:51 -0400

    Add basic level and collectible
    - Creates additional folders for Asset organization
    - Adds Spritesheet for level tiles
    - Creates Tile Pallete and Tiles for Ground
    - Creates "Collectible" script for items that can be collected
    - Creates "FoodCrumb" script for food items to be collected
    - Creates a test FoodCrumb that is a coffee mug for testing

---
+++ NotABug/Assets/Scripts/Character Testing/Collectible.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    protected string pickupMessage;
+    protected AudioClip audioClip;
+
+    private AudioSource audioSource;
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = audioClip;
+    }
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag != "Player")
+        {
+            return;
+        }
+
+        Debug.Log("Collision with player detected");
+        OnPickUp();
+    }
+
+    virtual protected void OnPickUp()
+    {
+        PlayPickupSound();
+        Vanish();
+        DisplayMessage();
+    }
+
+    private void PlayPickupSound()
+    {
+        if (null == audioSource)
+        {
+            return;
+        }
+
+        GetComponent<AudioSource>().Play();
+    }
+
+    private void Vanish()
+    {
+        Object.Destroy(gameObject);
+    }
+
+    private void DisplayMessage()
+    {
+
+    }
+}
